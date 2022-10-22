21002a95beb34cfc987a72c750daa6aa4fd9ae55
chrismorgar <111999839+chrismorgar@users.noreply.github.com>
2022-10-22 11:28:43 -0700

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,16 @@
+full_name = "Henry Bowe"
+nickname = "Henny"
+age = 25
+has_used_python = True
+
+hobbies = ["making music", "playing basketball", "playing video games", "watching anime", "watching sports", "hiking"]
+
+fav_things = {
+  "movie": "Undercover Brother",
+  "food": "steak",
+  "hobby": hobbies[0],
+  "anime": "One Piece",
+  "number": 13
+}
+
+all_vars = dict(vars())
